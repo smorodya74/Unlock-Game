0c218f60844cc36dbf6a2033fde8a023114bddc9
kkkiraaa <karina.hmelyova@gmail.com>
2024-06-27 18:03:19 +0500

    доделала скрипт, проверка на правильность пароля
    написала текстовый док в котором лежат все фразы Никиты на существующие комбинации, еще не проверяла, но от ошибок в коде избавила, так же там наконец-то появилась проверка на правильность пароля

--- Assets/Scripts/UIFirstL.cs
+++ Assets/Scripts/UIFirstL.cs
@@ -1,30 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using TMPro;
+using JetBrains.Annotations;
+using Unity.Burst.Intrinsics;
+using System.Linq;
+using System;
 
 public class UIFirstL : MonoBehaviour
 {
     public TextMeshProUGUI output;
     public TMP_InputField login;
     public TMP_InputField password;
+
     public void Button()
     {
-        if (login.text == "admin" && password.text == "admin") {
-            output.text = "Мдаааа…у “более опытных специалистов” убираю слово “более” ... возможно и “опытные”..." +
-        "\r\nТак делать нельзя. Логин и пароль должны иметь сложную структуру, чтобы хакерам было сложно подобрать. " +
-        "Часто у каждой организации свои генераторы паролей и логинов, которые строятся по определенным правилам." +
-        " Например первые буквы фио + дата рождения + год прихода на работу. \r\n"; ;
+        string path = @"c:\Unlock\PhrasesFirstL.txt";
+        string[] array = File.ReadAllLines(path);
+
+        if (login.text == "test" && password.text == "12345") {
+            output.text = array[0];
         }
-        else if(login.text == "test" && password.text == "12345")
+        else if(login.text == "user" && password.text == "user")
         {
-            output.text = "Я так понимаю это тестовый пользователь… " +
-                "но даже тестовых пользователей нужно удалять, либо ставить хорошую защиту, чтобы избежать лишней утечки информации.";
+            output.text = array[1];
+        }
+        else if (login.text == "guest" && password.text == "passwOrd")
+        {
+            output.text = array[2];
+        }
+        else if (login.text == "admin" && password.text == "admin")
+        {
+            output.text = array[3] + "\n" + array[4] + "\n" + array[5];
+        }
+        else if (isPasswordCorrect(password.text))
+        {
+            output.text = array[7];
         }
         else
         {
-            output.text = "Хмм... Что-то не то.\r\n" +
-                "Попробуй воспользоваться вариантом из списка.";
+            output.text = array[6];
         }
+        
+    }
+    public bool isPasswordCorrect(string pas)
+    {
+        if (pas.Length >= 8 && pas.Length <= 20)
+        {
+            Dictionary<string, char> types = new Dictionary<string, char>();
+            for (int i = 0; i < pas.Length; i++)
+            {
+                char c = pas[i];
+                if (Char.IsDigit(c)) { types.Add("digit", c); }
+                else if (Char.IsLower(c)) { types.Add("lower", c); }
+                else if (Char.IsUpper(c)) { types.Add("upper", c); }
+                else if (isChar(c)) { types.Add("chars", c); }
+            }
+            if (types.ContainsKey("digit") && types.ContainsKey("lower") && types.ContainsKey("upper") && types.ContainsKey("chars"))
+            {
+                return true;
+            }
+            else return false;
+        }
+        else return false;
+    }
+    public bool isChar(char ch)
+    {
+        if ((ch >= 33 && ch <= 47) || (ch >= 58 && ch <= 64) || (ch >= 91 && ch <= 96) || (ch >= 123 && ch <= 126)) { return true; }
+        else { return false; }
     }
 }
